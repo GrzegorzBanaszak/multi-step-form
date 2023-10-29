720c92e9840611e6d2b6a9ee013c1a526a4d7fcc
GrzegorzBanaszak <grzegorz.banaszak95@gmail.com>
2023-10-29 09:29:33 +0100

    Free month description for plan card

--- src/components/PlanCard.tsx
+++ src/components/PlanCard.tsx
@@ -19,18 +19,23 @@ const PlanCard: FunctionComponent<PlanCardPros> = ({ icon, title, price }) => {
   return (
     <div
       onClick={() => dispatch(selectPlan({ name: title, price: getPrice() }))}
-      className={`w-full border-[1px] border-gray-500/30 p-4 rounded-md flex gap-x-4 items-center lg:flex-col lg:items-start lg:gap-x-0 lg:gap-y-8 hover:border-marineBlue duration-300 cursor-pointer ${
+      className={`w-full border-[1px] border-gray-500/30 p-4 rounded-md  hover:border-marineBlue duration-300 cursor-pointer ${
         name === title && "bg-lightGray/20 border-marineBlue"
       }`}
     >
-      {/* icon */}
-      <div>{icon}</div>
-      {/* description */}
-      <div>
-        <div className="font-bold capitalize">{title}</div>
-        <div className="text-gray-600/40">
-          ${type === PlanType.MONTHLY ? price : price * 10}/
-          {type === PlanType.MONTHLY ? "mo" : "yer"}
+      <div className="flex gap-x-4 items-center lg:flex-col lg:items-start lg:gap-x-0 lg:gap-y-8">
+        {/* icon */}
+        <div>{icon}</div>
+        {/* description */}
+        <div>
+          <div className="font-bold capitalize">{title}</div>
+          <div className="text-gray-600/40">
+            ${type === PlanType.MONTHLY ? price : price * 10}/
+            {type === PlanType.MONTHLY ? "mo" : "yer"}
+          </div>
+          {type === PlanType.YEARLY && (
+            <div className="text-marineBlue">2 months free</div>
+          )}
         </div>
       </div>
     </div>
